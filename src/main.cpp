39233d6136dea8db3ad0cd5dc676d2d907ccdb47
Ryan ODell <ryanodell2013@gmail.com>
2024-06-11 11:24:07 -0700

    Starting over :(

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,41 @@
-#include <glad.h>
-//#define GLFW_DLL
-#include "pch.h"
-#include "core/game.h"
 #include <iostream>
 
-
-
 int main() {
-    //mystic::logger::init();
-    //MYSTIC_INFO("Boot.");
     std::cout << "test" << std::endl;
-    std::cout << "test2" << std::endl;
-    std::cout << "test3" << std::endl;
-    // MYSTIC_INFO("Boot.");
-    // mystic::game mainGame;
-    // mainGame.run();
-    // MYSTIC_INFO("Game closed.");
-    return 0;
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//original - after this is all figured out =/
+// #include <glad.h>
+// //#define GLFW_DLL
+// #include "pch.h"
+// #include "core/game.h"
+// #include <iostream>
+
+
+
+// int main() {
+//     mystic::logger::init();
+//     mystic::logger::getInstance()->trace("sup");
+//     //MYSTIC_INFO("Boot.");
+//     std::cout << "test" << std::endl;
+//     std::cout << "test2" << std::endl;
+//     // MYSTIC_INFO("Boot.");
+//     // mystic::game mainGame;
+//     // mainGame.run();
+//     // MYSTIC_INFO("Game closed.");
+//     return 0;
+// }
