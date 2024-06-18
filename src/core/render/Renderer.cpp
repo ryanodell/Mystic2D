46b814ddafa10cd1d7f6ed9b3ba8aeb7c8181d3c
Ryan ODell <ryanodell2013@gmail.com>
2024-06-18 15:50:01 -0700

    Updated renderer to make use of vertex stuffs

--- src/core/render/Renderer.cpp
+++ src/core/render/Renderer.cpp
@@ -1,5 +1,20 @@
 #include "renderer.h"
 
-void Mystic::Renderer::Draw() {
-    
+namespace Mystic{
+void Renderer::Clear() const {
+    GLCall(glClear(GL_COLOR_BUFFER_BIT));
 }
+void Renderer::Draw(const VertexArray& va, const IndexBuffer* ib, const Shader& shader) const {
+    shader.Use();
+    va.Bind();
+
+    //TODO!!!! I SHOULD BE ABLE TO DO ib.Bind() and ib.GetCount()!! Need to investigate why
+    ib->Bind();
+    //auto test = ib.GetCount();
+
+    glDrawElements(GL_TRIANGLES, ib->GetCount(), GL_UNSIGNED_INT, nullptr);
+    //GLCall(glDrawElements(GL_TRIANGLES, ib->GetCount(), GL_UNSIGNED_INT, nullptr));
+    //ib.Bind();
+    //GLCall( glDrawElements(GL_TRIANGLES, ib->GetCount(), GL_UNSIGNED_INT, nullptr) );
+}
+}  // namespace Mystic
