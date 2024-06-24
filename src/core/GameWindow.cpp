3143fbf7a43925691bff510613f34ddc21d0c012
Ryan ODell <ryanodell2013@gmail.com>
2024-06-24 16:23:36 -0700

    Removed comments, final cleanup

--- src/core/GameWindow.cpp
+++ src/core/GameWindow.cpp
@@ -2,13 +2,8 @@
 
 namespace Mystic {
     GameWindow::GameWindow(const std::string &windowName, int width, int height) {
-        // glfwInit();
-        // glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-        // glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-        // glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
         m_internalWindow = glfwCreateWindow(width, height, windowName.c_str(), NULL, NULL);
         if(m_internalWindow == NULL) {
-            //MYSTIC_CRITICAL("Failed to create Glfw window.");
             glfwTerminate();
         }
         glfwMakeContextCurrent(m_internalWindow);        
@@ -22,12 +17,10 @@ namespace Mystic {
                 gw->frameBufferSizeCallback(window, width, height);
             }
         });
-        //MYSTIC_INFO("Successfully created Glfw window");
     }
     bool GameWindow::WindowShouldClose() {
         bool shouldClose = glfwWindowShouldClose(m_internalWindow);
         return shouldClose;
-        //return glfwWindowShouldClose(m_internalWindow);
     }
     void GameWindow::frameBufferSizeCallback(GLFWwindow *window, int width, int height)
     {
