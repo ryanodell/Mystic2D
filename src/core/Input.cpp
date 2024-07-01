cf685cede55bcf79e013b3caecaafb66c16f24c7
Ryan ODell <ryanodell2013@gmail.com>
2024-07-01 10:23:48 -0700

    exported methods to cpp

--- src/core/Input.cpp
+++ src/core/Input.cpp
@@ -1,2 +1,33 @@
 #include "Input.h"
 
+namespace Mystic {
+
+InputManager::InputManager(GLFWwindow* window) : window(window) {
+    glfwSetWindowUserPointer(window, this);
+    glfwSetKeyCallback(window, keyCallback);
+}
+
+void InputManager::Update() {
+    for (auto& [key, pressed] : m_keyPressed) {
+        if (pressed) {
+            pressed = false;
+        }
+    }
+}
+
+bool InputManager::IsKeyPressed(int key) const {
+    auto it = m_keyPressed.find(key);
+    if (it != m_keyPressed.end()) {
+        return it->second;
+    }
+    return false;
+}
+
+bool InputManager::IsKeyHeld(int key) const {
+        auto it = m_keyHeld.find(key);
+        if (it != m_keyHeld.end()) {
+            return it->second;
+        }
+        return false;
+    }
+}
