6028e55ff77a46cdf493f40a542dea61e53151ad
Ryan ODell <ryanodell2013@gmail.com>
2024-07-15 10:19:05 -0700

    Created class for random number generation

--- src/core/Utils.h
+++ src/core/Utils.h
@@ -1,10 +1,25 @@
 #ifndef UTILS_H
 #define UTILS_H
-#include "../GLM/glm.hpp"
+#include <random>
+
 #include "../Constants.h"
+#include "../GLM/glm.hpp"
 
 namespace Mystic {
-    glm::vec4 GetColorVec4(Color color);
-}
+glm::vec4 GetColorVec4(Color color);
+
+class Random {
+   public:
+    Random() {}
+    Random(int min, int max) : rng(rd()), dist(min, max) { }
+    int GetRandomNumber() {
+        return dist(rng);
+    }
+   private:
+    std::random_device rd;                    // Seed generator
+    std::mt19937 rng;                         // Mersenne Twister random number generator
+    std::uniform_int_distribution<int> dist;  // Distribution
+};
+}  // namespace Mystic
 
 #endif
