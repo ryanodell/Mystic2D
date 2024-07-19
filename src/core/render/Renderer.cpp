200e3600d1783eb0a876aa23a9f3b7ca4bcf9a95
Ryan ODell <ryanodell2013@gmail.com>
2024-07-19 06:17:17 -0700

    Loading chars up into memory

--- src/core/render/Renderer.cpp
+++ src/core/render/Renderer.cpp
@@ -190,6 +190,30 @@ void Renderer::populateCharacters() {
         if (FT_Load_Char(face, c, FT_LOAD_RENDER)) {
             MYSTIC_WARN("Failed to load " + std::string(1, static_cast<char>(c)) + " char");
         }
+
+        unsigned int texture;
+        glGenTextures(1, &texture);
+        glBindTexture(GL_TEXTURE_2D, texture);
+        glTexImage2D(
+            GL_TEXTURE_2D,
+            0,
+            GL_RED,
+            face->glyph->bitmap.width,
+            face->glyph->bitmap.rows,
+            0,
+            GL_RED,
+            GL_UNSIGNED_BYTE,
+            face->glyph->bitmap.buffer);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+        Character character = {
+            texture,
+            glm::ivec2(face->glyph->bitmap.width, face->glyph->bitmap.rows),
+            glm::ivec2(face->glyph->bitmap_left, face->glyph->bitmap_top),
+            face->glyph->advance.x};
+        m_characters.insert(std::pair<char, Character>(c, character));
     }
 }
 }  // namespace Mystic
